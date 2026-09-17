7f409a771353e9f6068b89d4e2b3027f4209b901
Kunal Parit <kunalparit92@gmail.com>
2026-09-18 01:28:31 +0530

    Day 1

---
+++ day1/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="card">
+        <h1>React Fundamentals</h1>
+        <h2>Day 1 - JSX Basics</h2>
+        <p>Welcome to my React project</p>
+        <p className="name">My Name is Kunal Parit</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
